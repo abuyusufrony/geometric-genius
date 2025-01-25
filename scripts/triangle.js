f6d9c66bd29edd273368553bd89a9c43f6bb0fef
abuyusufrony <abuyusufrony@gmail.com>
2025-01-25 22:47:19 +0600

    trinagle

--- scripts/triangle.js
+++ scripts/triangle.js
@@ -1,3 +1,19 @@
 function calculateTrainagle() {
-    console.log('triangle are clicked');
+
+    const baseTriangle = document.getElementById('base');
+    const newbaseTriangle = baseTriangle.value;
+    const baseFloat = parseFloat(newbaseTriangle);
+    console.log(baseFloat);
+
+    const triangleHeight = document.getElementById('height');
+    const heightValue = triangleHeight.value;
+    const hfloat = parseFloat(heightValue);
+    console.log(hfloat);
+    const area = 0.5 * baseFloat * hfloat;
+    console.log('Trainagle are:', area);
+    const display = document.getElementById('areap');
+    display.innerText = area;
+
+
+
 }
