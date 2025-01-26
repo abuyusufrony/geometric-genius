30bf530e85f8934d497e2d9eb569eb015e5f4bcc
abuyusufrony <abuyusufrony@gmail.com>
2025-01-27 00:36:26 +0600

    reuse-function

--- scripts/rom.js
+++ scripts/rom.js
@@ -1,13 +1,28 @@
-function getRombosValue() {
-    const romValue = document.getElementById('rombus');
-    const romText = romValue.value;
-    const romw = parseFloat(romText);
-    console.log(romw)
-    const romHeight = document.getElementById('romboH');
-    const romHtext = romHeight.value;
-    const romH = parseFloat(romHtext);
-    console.log(romH)
-    const rombosArea = romw * romH;
-    console.log('Rombus are are :', rombosArea);
+// function getRombosValue() {
+//     const romValue = document.getElementById('rombus');
+//     const romText = romValue.value;
+//     const romw = parseFloat(romText);
+//     console.log(romw)
+//     const romHeight = document.getElementById('romboH');
+//     const romHtext = romHeight.value;
+//     const romH = parseFloat(romHtext);
+//     console.log(romH)
+//     const rombosArea = romw * romH;
+//     console.log('Rombus are are :', rombosArea);
 
+// }
+
+function getRombosValue() {
+    const frombus = getValueById('rombus');
+    console.log(frombus);
+    const hRombos = getValueById('romboH');
+    console.log(hRombos);
+}
+
+function getValueById(inPutFieldId) {
+    const inPutField = document.getElementById(inPutFieldId);
+    const romT = inPutField.value;
+    const floatRombus = parseFloat(romT);
+    inPutField.value = '';
+    return floatRombus;
 }
